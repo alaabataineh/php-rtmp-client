9d43b5a2e170c5dbca235bd6b93eb87c307fff93
qwantix <qwantix@63393608-ba17-11de-8be4-e1425cda3908>
2009-10-26 22:54:08 +0000

    Fix unique call bug Remove pending call management method call return result of rpc cleaning code, and some others improvements...

--- trunk/RtmpClient/main.php
+++ trunk/RtmpClient/main.php
@@ -1,11 +1,8 @@
 <?php
 require "RtmpClient.class.php";
-
+require "debug.php";
 $client = new RtmpClient();
-$client->connect("localhost","myApp",1935);
-$client->call("getId",null, onId);
-function onId(RtmpOperation $op)
-{
-	print "ID!";
-}
+$client->connect("localhost","myApp");
+$result = $client->call("myMethod");
+var_dump($result);
 
